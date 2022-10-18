d4a8f4599e14f71ae27aaf05d254426eb52d34fa
gabsborges <gabriel.borges_96@hotmail.com>
2022-10-18 17:34:27 -0300

    adicionado leadlovers

--- src/components/Contato/Orcamento/Orcamento.js
+++ src/components/Contato/Orcamento/Orcamento.js
@@ -27,7 +27,48 @@ function Orcamento() {
                     </div>
                 </div>
                 <div className='orcamento-form-box'>
-                    <form action="" method="get" className="orcamento-form-area">
+                    <form className="orcamento-form-area" action="https://paginas.rocks/pages/index/650978" method="post" >
+                        <input id="id" name="id" type="hidden" value="650978" />
+                        <input id="mid" name="mid" type="hidden" value="650978" />
+                        <input id="pid" name="pid" type="hidden" value="20960542" />
+                        <input id="list_id" name="list_id" type="hidden" value="650978" />
+                        <input id="provider" name="provider" type="hidden" value="leadlovers" />
+
+                        <div className="orcamento-form-area">
+                            <label htmlFor="email">E-mail:</label>
+                            <input className="form-control form-ll" id="email" name="email" placeholder="Informe o seu email" type="text" />
+                        </div>
+                        <div className="orcamento-form-area">
+                            <label htmlFor="name">Nome:</label>
+                            <input className="form-control form-ll" id="name" name="name" placeholder="Informe o seu nome" type="text" />
+                        </div>
+                        <div className="orcamento-form-area">
+                            <label htmlFor="phone">Telefone:</label>
+                            <input className="form-control form-ll" id="phone" name="phone" placeholder="Informe o telefone" type="text" />
+                        </div>
+                        <div className="orcamento-form-area">
+                            <label htmlFor="company">Empresa:</label>
+                            <input className="form-control form-ll" id="company" name="company" placeholder="Informe o nome da empresa" type="text" />
+                        </div>
+                        <div className="orcamento-form-area">
+                            <label htmlFor="message">Mensagem:</label>
+                            <textarea className="form-control form-ll text-form-area" id="message" name="message" placeholder="Digite uma mensagem ou pergunta" maxLength="500" ></textarea>
+                        </div>
+
+                        <div className="checkbox-list orcamento-form-area-checkbox" data-required="true">
+                            <div className="checkbox">
+                                <label className="fixed">
+                                    <input id="LGPDValidation" name="LGPDValidation" type="checkbox" value="1" />&nbsp;Sim, desejo receber e-mails sobre artigos, noticias, tendências ou ofertas. O consentimento pode ser retirado a qualquer momento clicando no link de cancelamento de inscrição presente em todas as comunicações por e-mail.
+                                </label>
+                            </div>
+                        </div>
+                        <div className="orcamento-form-area-submit">
+                            <button className="btn btn-danger" type="submit">Enviar!</button>
+                        </div>
+                        <input type="hidden" id="source" name="source" value="" />
+                        <img className='displayNone' src="https://llimages.com/redirect/redirect.aspx?A=V&p=20960542&m=650978" />
+                    </form>
+                    {/* <form action="" method="get" className="orcamento-form-area">
                         <div className="orcamento-form-area">
                             <label for="email">E-mail:</label>
                             <input type="email" name="email" id="email" placeholder='Informe seu e-mail' required />
@@ -55,11 +96,11 @@ function Orcamento() {
                         <div className="orcamento-form-area-submit">
                             <input type="submit" value="Enviar!" />
                         </div>
-                    </form>
+                    </form> */}
                 </div>
             </div>
             <div className='orcamento-maps'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.517527680389!2d-46.634750685432635!3d-23.585765584670774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a2aa7ceaac1%3A0x30e9b2040cd24ea0!2sR.%20Bartolomeu%20de%20Gusm%C3%A3o%2C%20290%20-%20Vila%20Mariana%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004111-020!5e0!3m2!1spt-BR!2sbr!4v1665152388256!5m2!1spt-BR!2sbr" width="100%" height="320" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.517527680389!2d-46.634750685432635!3d-23.585765584670774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a2aa7ceaac1%3A0x30e9b2040cd24ea0!2sR.%20Bartolomeu%20de%20Gusm%C3%A3o%2C%20290%20-%20Vila%20Mariana%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004111-020!5e0!3m2!1spt-BR!2sbr!4v1665152388256!5m2!1spt-BR!2sbr" width="100%" height="320" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     )
