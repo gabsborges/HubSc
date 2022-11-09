f1bdf66bf52dc5aa26dec431cf87d45aee0800cb
gabsborges <gabriel.borges_96@hotmail.com>
2022-11-09 13:23:54 -0300

    teste no pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Acessibilidade from './pages/Acessibilidade';
 import Carreira from './pages/Carreira';
@@ -8,29 +7,51 @@ import Contato from './pages/Contato';
 import NoPage from './pages/NoPage';
 import Privacidade from './pages/Privacidade';
 import SobreAHub from './pages/SobreAHub';
-import Layout from './pages/Layout';
 import ServicosBpoBps from './pages/BpoBps';
 import ServicoConsultoria from './pages/Consultoria';
 import ServicosOnDemand from './pages/OnDemand';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: "/contato",
+    element: <Contato />
+  },
+  {
+    path: "/sobre-a-hub",
+    element: <SobreAHub />
+  },
+  {
+    path: "/servicos-bpo-bps",
+    element: <ServicosBpoBps />
+  },
+  {
+    path: "/servicos-consultoria",
+    element: <ServicoConsultoria />
+  },
+  {
+    path: "/servicos-on-demand",
+    element: <ServicosOnDemand/>
+  },
+  {
+    path: "/carreira",
+    element: <Carreira/>
+  },
+  {
+    path: "/privacidade-e-cookies",
+    element: <Privacidade/>
+  },
+  {
+    path: "/acessibilidade",
+    element: <Acessibilidade/>
+  },
+  {
+    path: "*",
+    element: <NoPage/>
+  }
+])
 
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route index element={<Home />} />
-          <Route path="/contato" element={<Contato />} />
-          <Route path="/sobre-a-hub" element={<SobreAHub />} />
-          <Route path="/servicos-bpo-bps" element={<ServicosBpoBps />} />
-          <Route path="/servicos-consultoria" element={<ServicoConsultoria />} />
-          <Route path="/servicos-on-demand" element={<ServicosOnDemand/>} />
-          <Route path="/carreira" element={<Carreira/>} />
-          <Route path="/privacidade-e-cookies" element={<Privacidade/>} />
-          <Route path="/acessibilidade" element={<Acessibilidade/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+export { router }
